ec8150195a6b14f42a4fee390b1f352a1fa690a6
Mauro <102637370+Mauro010BR@users.noreply.github.com>
2022-08-27 12:20:09 -0300

    Add files via upload

---
+++ Exercício B.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercício_B
+{
+    internal class Program
+    {
+
+        static void Potencia(double X,double Y)
+        {
+            double Result = Math.Pow(X, Y);
+            Console.WriteLine($"O resultado é {Result} ");
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos fazer uma Potência");
+            Console.Write("Digite a Base: ");
+            double Base = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o expoente: ");
+            double exp = double.Parse(Console.ReadLine());
+
+            Potencia(Base, exp);
+
+            Console.ReadKey();
+        }
+    }
+}
